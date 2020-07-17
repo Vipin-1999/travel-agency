93247077fd8276be376c04780e8a0065d771d962
Vipin R Bharadwaj <vipinrbharadwaj@gmail.com>
2020-07-17 09:19:46 +0530

    Changed Access Token

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const crossProduct = require('./crossProduct')
 const fetchData = require('./fetchData.js')
 
 const cities = ['Bangalore', 'Kolkata', 'Delhi', 'Hyderabad', 'Chennai']
-const searchQuery = ['Travel Agency', 'Travel Agents', 'International Travel Agent']
+const searchQuery = ['Travel%20Agency', 'Travel%20Agents', 'International%20Travel%20Agent']
 
 crossProduct(cities, searchQuery).map(x => {
-    let url = resource.baseURL + searchQuery[0] + cities[0] + resource.queryString + accessToken;
+    let url = resource.baseURL + x[1] + '%20' + x[0] + resource.queryString + accessToken;
     fetchData(url, x)
 });
